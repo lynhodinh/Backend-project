d280967d2875ff1a049a0a100cdd0412e0465b45
lynhodinh <lhodinh@gmail.com>
2023-04-17 11:20:45 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -16,8 +16,14 @@ const {
 
 const { deleteComment } = require("./Controllers/commentController.js");
 
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
+
+app.get("/api");
 
 app.get("/api/categories", getCategories);
 
